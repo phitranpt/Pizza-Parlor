6d7bd50654a674f3e42da70a3ec89844ee9650b4
Lex Chudzik <chudzik.alex@gmail.com>
2018-11-01 13:55:23 -0500

    reducers in progress

--- src/index.js
+++ src/index.js
@@ -3,4 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// GET PizzaReducer
+const getPizzaReducer = (state=[ ], action) => {
+    console.log('In Get Pizza Reducer');
+    if (action.type === 'GET_PIZZA') {
+        state = action.payload;
+    }
+    return state;
+}
+
+const orderReducer = (state={}, action) => {
+    if (action.type === 'ADD_PIZZA') {
+        //add pizzas to state
+        state = action.payload 
+    } else if (action.type === 'ADD_CUSTOMER') {
+        state = 
+        //add customer to state
+    } else if (action.type === 'CHECKOUT') {
+        //send state to db as order
+        
+        state = {};
+    }
+    return state
+}
+
+// Store
+const storeInstance = createStore(
+    combineReducers({
+      getPizzaReducer,
+      orderReducer
+    }),
+    applyMiddleware(logger)
+  )
+
+ReactDOM.render(<Provider store={ storeInstance }><App /></Provider>, document.getElementById('root'));
