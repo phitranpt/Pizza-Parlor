551be040127b591a95366f94e6204fdc9e519443
Lex Chudzik <chudzik.alex@gmail.com>
2018-11-01 11:41:37 -0500

    added checkout, started building

---
+++ src/components/Checkout/Checkout.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import {withRouter} from 'react-router-dom';
+import axios from 'axios';
+
+class Checkout extends Component {
+
+    checkout = () => {
+        console.log('clicked checkout');
+
+        //send order to database
+        axios.post({
+            url: '/api/order/'
+            data: ''
+        }).then( (response) => {
+            //confirmation dialog
+        alert('Order Sent')
+        
+        //send back to step one
+        this.props.history.push('/????');
+        }).catch( (error) => {
+            console.log('error posting ');
+
+        })
+
+    }
+
+    render() {
+      return (
+        <div>
+          <h3>Step 3: Checkout</h3>
+          <button onClick={this.checkout}/>
+        </div>
+      );
+    }
+  }
+  
+export default Checkout;
