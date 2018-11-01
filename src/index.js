b8a82ef1a5dd78c1bd84c247f97270ec0aa7342a
Lex Chudzik <chudzik.alex@gmail.com>
2018-11-01 14:03:20 -0500

    commented out state changes in reducer

--- src/index.js
+++ src/index.js
@@ -19,9 +19,12 @@ const getPizzaReducer = (state=[ ], action) => {
 const orderReducer = (state={}, action) => {
     if (action.type === 'ADD_PIZZA') {
         //add pizzas to state
-        state = action.payload 
+        console.log(action.payload);
+        //state = action.payload 
     } else if (action.type === 'ADD_CUSTOMER') {
-        state = 
+        console.log(action.payload);
+        
+        //state = {...state}
         //add customer to state
     } else if (action.type === 'CHECKOUT') {
         //send state to db as order
