5422fe177fd5b0f6d838e5b9d78eec61788e6133
Cooper Miller <coopermllr@gmail.com>
2018-11-01 16:11:06 -0500

    reset app - not up to date with group

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import './App.css';
-// import CustomerInfo from '../CustomerInfo/CustomerInfo'
-import GetPizza from '../GetPizza/GetPizza'
 
 class App extends Component {
   render() {
@@ -12,9 +10,8 @@ class App extends Component {
           <h1 className="App-title">Prime Pizza</h1>
         </header>
         <br/>
-        <img alt="" src="images/pizza_photo.png"/>
+        <img src="images/pizza_photo.png"/>
         <p>Pizza is great.</p>
-        <GetPizza />
       </div>
     );
   }
