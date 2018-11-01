8f0300800193eb9d0053d960bf0345fac30f60fa
William Harrison <wm.harrison@gmail.com>
2018-11-01 13:56:32 -0500

    completed basic view layout and table GET

--- src/components/AdminView/AdminView.js
+++ src/components/AdminView/AdminView.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class AdminView extends Component {
-
+state= {
+    orders: [],
+}
     getOrders = () => {
         axios.get('/api/order')
         .then((response) => {
             console.log('The orders have arrived from the GET request!', response.data);
-            // this.props.dispatch({type: ALL_ORDERS, payload: response.data})
+            const orders = response.data;
+            this.setState({orders});
         }).catch((error) => {
             alert('Unable to GET all orders!');
         })
+    }
+
+    componentDidMount() {
+        this.getOrders();
     }
 
     render() {
@@ -21,15 +28,17 @@ class AdminView extends Component {
                 </header>
                 <table>
                     <thead>
-                    <th>Name</th><th>Time Order Placed</th><th>Type</th><th>Cost</th>
+                        <tr>
+                        <th>Name</th><th>Time Order Placed</th><th>Type</th><th>Cost</th>
+                        </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                        <td></td><td></td><td></td><td></td>
-                        </tr>
+                        {this.state.orders.map(orders => (
+                        <tr key={orders.id}>
+                        <td>{orders.customer_name}</td><td>{orders.time}</td><td>{orders.type}</td><td>{orders.total}</td>
+                        </tr>))}
                     </tbody>
                 </table>
-
             </div>
         );
     }
